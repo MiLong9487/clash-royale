b05ea30e48c4e5402ddc1e116d4beab27f82ea88
hugo0329 <hugo940329@gmail.com>
2023-04-01 21:26:13 +0800

    Bottoon

--- client.py
+++ client.py
@@ -10,3 +10,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+cardA = python.image.load()
+class Button:
+    def __init__(self,image,width,height,position):
+        self.top_rect = pygame.rect(position,(width,height))
+        self.top_color = (255,228,181)
+    
+    def draw(self):
+        pygame.draw.rect(screen,self.top_color,self.top_rect)
+        screen.blit(cardA,center=self.top_rect.center)
