1974a4cd64906109b6278767d8fb6c455287f38c
hugo0329 <129491675+hugo0329@users.noreply.github.com>
2023-04-01 18:51:19 +0800

    Update client.py

--- client.py
+++ client.py
@@ -10,8 +10,3 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-class Button:
-    def __init__(self,text,width,height,position):
-        self.top_rect = pygame.rect(position,(width,height))
-        self.top_color = (255,228,181)
