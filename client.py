b04d1b8c7e2fa2942b06484a4940da6524c554ce
hugo0329 <hugo940329@gmail.com>
2023-04-02 22:32:37 +0800

    bbb

--- client.py
+++ client.py
@@ -12,17 +12,21 @@ class Button(pygame.sprite.Sprite):
         self.image = pygame.Surface([width,height])
         self.image.fill(250,250,250)
         self.rect = self.image.get_rect()
-        self.rect.center = [pos_x,pos_y]
+        self.rect.center = (pos_x,pos_y)
 
 button_group = pygame.sprite.Group()
 button = Button(50,50,100,100)
 button_group.add(button)
 
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        
+
+        
     pygame.display.flip()
     button_group.draw(screen)
 
