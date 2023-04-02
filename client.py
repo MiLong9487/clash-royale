a5ac20baa8252c80a1435f9bb0a1a19e18960e87
hugo0329 <hugo940329@gmail.com>
2023-04-02 18:15:40 +0800

    button

--- client.py
+++ client.py
@@ -2,21 +2,21 @@ import pygame,sys
 from constants import *
 
 pygame.init()
-screen = pygame.display.set_mode([500,600])
+screen = pygame.display.set_mode([300,640])
 
 running = True
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self,width,height,pos_x,pos_y,color):
+    def __init__(self,width,height,pos_x,pos_y):
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("")
         self.image = pygame.Surface([width,height])
-        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x,pos_y]
     
 
 button_group = pygame.sprite.Group()
-button = Button(50,50,100,100,(250,250,250))
+button = Button(50,50,100,100)
 button_group.add(button)
 
 while running:
